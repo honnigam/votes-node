ab8cea8805156f4d698d5a7afe95c509d1c190fa
Lucas Barcelos <lucasvm87@gmail.com>
2024-02-13 21:17:04 -0300

    add: create route to a register a new poll

--- src/http/routes/createVote.ts
+++ src/http/routes/createVote.ts
@@ -2,21 +2,31 @@ import { z } from "zod"
 import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
 
-
-export async function createVote (app: FastifyInstance) {
+export async function createVote(app: FastifyInstance) {
   app.post('/votes', async (request, reply) => {
     const createVoteBody = z.object({
-      title: z.string()
+      title: z.string(),
+      options: z.array(z.string()),
     })
-  
-   const { title } = createVoteBody.parse(request.body)
-  
-    const vote = await prisma.vote.create ({
-    data: {
-      title,
-    }
-   })
-  
-    return reply.status(201).send({voteId: vote.id})
+
+    const { title, options } = createVoteBody.parse(request.body)
+
+    const vote = await prisma.vote.create({
+      data: {
+        title,
+        optionsVote: {
+          createMany: {
+            data: options.map((option) => {
+              return {
+                title: option,
+              }
+            })
+          }
+        }
+      }
+    })
+
+    return reply.status(201).send({ voteId: vote.id })
   })
+
 }
